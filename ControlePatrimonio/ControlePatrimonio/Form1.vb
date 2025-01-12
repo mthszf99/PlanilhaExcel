2be6e0f701b9ef9798ab563a9d64d4aa76643c74
Rafael Carvalho <rafaelgcarvalho95@gmail.com>
2025-01-11 22:11:52 -0300

    desenvolvido executavel e resolvido bug de instancia

---
+++ ControlePatrimonio/ControlePatrimonio/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim xlWb As Object
+        Dim xlApp As Object
+        Dim xlWbName As String
+        Dim xlWbPath As String
+
+        On Error Resume Next
+
+        xlApp = CreateObject("Excel.Application")
+
+        xlWbName = "Planilha_Segura.xlsb"
+
+        xlWbPath = Application.StartupPath
+
+        Me.Hide()
+
+        xlWb = xlApp.workbooks.open(xlWbPath & "\" & xlWbName)
+
+        xlWb = Nothing
+        xlApp = Nothing
+        xlWbName = Nothing
+        xlWbPath = Nothing
+
+        Application.Exit()
+
+
+
+
+
+    End Sub
+End Class
